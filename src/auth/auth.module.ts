d902e80dfd5fe0de038a81adf01454eaecfa7612
Vien Dinh <maxvien@gmail.com>
2019-11-06 16:29:25 +0700

    Update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
-import { FirebaseStrategy } from './firebase.strategy';
 import { User } from '../graphql/user/user.model';
+import { FirebaseStrategy } from './firebase.strategy';
 import { FirebaseService } from '../services/firebase.service';
 
 @Module({
