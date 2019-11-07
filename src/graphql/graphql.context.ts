48219ce269258e1ca254b842a89c1a7874d76074
Maxvien <maxvien@Maxviens-MacBook-Air.local>
2019-11-07 12:44:22 +0700

    Update ContextType

--- src/graphql/graphql.context.ts
+++ src/graphql/graphql.context.ts
@@ -4,4 +4,5 @@ export interface ContextType {
   req: {
     user: User;
   };
+  res: {};
 }
--- src/graphql/graphql.module.ts
+++ src/graphql/graphql.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { UserModule } from './user/user.module';
+import { ContextType } from './graphql.context';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       installSubscriptionHandlers: true,
       autoSchemaFile: __dirname + '/schema.gql',
-      context: ({ req, res }) => ({ req, res }),
+      context: ({ req, res }): ContextType => ({ req, res }),
     }),
     UserModule,
   ],
