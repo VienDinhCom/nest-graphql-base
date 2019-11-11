0b9559f1a8a4e552cfcf05eed636d8022d0ed08b
Maxvien <maxvien@Maxviens-MacBook-Air.local>
2019-11-11 13:17:06 +0700

    Disable playground on production

--- src/graphql/graphql.module.ts
+++ src/graphql/graphql.module.ts
@@ -11,7 +11,6 @@ import { roles } from '../auth/auth.roles';
       installSubscriptionHandlers: true,
       autoSchemaFile: __dirname + '/schema.gql',
       context: ({ req, res }): ContextType => ({ req, res }),
-      playground: true,
     }),
     AccessControlModule.forRoles(roles),
     UserModule,
